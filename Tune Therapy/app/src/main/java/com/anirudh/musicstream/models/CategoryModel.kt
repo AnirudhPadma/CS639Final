805d7b84f7c7545c7130f47fe4423d4b090601b1
AnirudhPadma <anirudhpadma@gmail.com>
2024-05-04 15:18:22 -0400

    AnirudhPadma

--- Tune Therapy/app/src/main/java/com/anirudh/musicstream/models/CategoryModel.kt
+++
@@ -1,9 +0,0 @@
-package com.anirudh.musicstream.models
-
-data class CategoryModel(
-    val name : String,
-    val coverUrl : String,
-    val songs : List<String>
-) {
-    constructor() : this("","", listOf())
-}
